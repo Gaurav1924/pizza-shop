7931784178663ef8b0079d371e03eca0bfe938ed
Gaurav1924 <gauravkshatriya99@gmail.com>
2024-03-15 15:04:19 +0530

    comments removed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React, { useEffect, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import './index.css';
-import PizzaForm from './components/PizzaForm';
-import MainSection from './components/MainSection';
-import { updateOrdersArr } from './store/orderSlice';
-import PizzaSection from './components/PizzaSection';
+import "./index.css";
+import PizzaForm from "./components/PizzaForm";
+import MainSection from "./components/MainSection";
+import { updateOrdersArr } from "./store/orderSlice";
+import PizzaSection from "./components/PizzaSection";
 
 function App() {
   const [orders, setOrders] = useState([]);
-  const orderReducer = useSelector(store => store.order) 
-  // console.log(orderReducer) 
-  
-  // const dispatch = useDispatch()  
-
- 
-
-  // const getNextStage = (currentStage) => {
-  //   switch (currentStage) {
-  //     case 'Order Placed':
-  //       return 'Order in Making';
-  //     case 'Order in Making':
-  //       return 'Order Ready';
-  //     case 'Order Ready':
-  //       return 'Order Picked';
-  //     default:
-  //       return currentStage;
-  //   }
-  // };
-
-
-  
+  const orderReducer = useSelector((store) => store.order);
   return (
-
     <div className="container">
       <PizzaForm />
-      <PizzaSection/>
+      <PizzaSection />
       <div>
         <h2>Main Section</h2>
-        <MainSection
-          orders={orders}
-        />
+        <MainSection orders={orders} />
       </div>
     </div>
   );
